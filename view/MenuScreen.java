c0971a30b9bd928760529e5888436d44b55ecf02
LFsolar <sunlisa@gmail.com>
2020-08-17 13:18:51 -0500

    menu done

--- view/MenuScreen.java
+++ view/MenuScreen.java
@@ -1,6 +1,13 @@
 package view;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MenuScreen {
 
@@ -8,10 +15,23 @@ public class MenuScreen {
 
 	public MenuScreen(JFrame window) {
 		this.window = window;
+		window.setTitle("Menu");
 	}
 
 	public void init () {
-		
+
+		Container cp = window.getContentPane();
+		JPanel panel = new JPanel();
+
+		JButton calcButton = new JButton("Calculator Simulator");
+
+		JButton piggybankButton = new JButton("Piggy Bank Simulator");
+
+		panel.setLayout(new GridLayout(2,1));
+		panel.setPreferredSize(new Dimension(400, 200));
+		panel.add(calcButton);
+		panel.add(piggybankButton);
+		cp.add(BorderLayout.CENTER, panel);
 	}
 	
 }
