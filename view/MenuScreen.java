dc6c22a8feb2b95c6d19e9a1b966a36945435178
LFsolar <sunlisa@gmail.com>
2020-08-17 12:51:32 -0500

    initial commit

---
+++ view/MenuScreen.java
@@ -0,0 +1,17 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class MenuScreen {
+
+	private JFrame window;
+
+	public MenuScreen(JFrame window) {
+		this.window = window;
+	}
+
+	public void init () {
+		
+	}
+	
+}
